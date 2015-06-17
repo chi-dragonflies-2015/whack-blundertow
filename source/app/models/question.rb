ef7caf5d71905851c70082899bcc50baa8e20b1d
Michael Kunc & Nick Siefken & Peter Macaluso <pair+michaelkunc.nsiefken.pmacaluso3@devbootcamp.com>
2015-06-17 10:17:11 -0500

    5 models

---
+++ source/app/models/question.rb
@@ -0,0 +1,6 @@
+class Question < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :best_answer, class_name: "Answer"
+  has_many :comments, as: :commentable
+  has_many :votes, as: :votable
+end
