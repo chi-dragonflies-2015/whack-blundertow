e4e250d2851374a1127ee6d6b585afcbd9affa8a
Michael Kunc & Nick Siefken & Peter Macaluso <pair+michaelkunc.nsiefken.pmacaluso3@devbootcamp.com>
2015-06-18 09:40:06 -0500

    login and out

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,14 +1,26 @@
 post '/sessions' do
-  if true#User.authenticate(params[:username], params[:password])
+  if User.authenticate(params[:username], params[:password])
     this_user = User.find_by(username: params[:username])
     session[:user_id] = this_user.id
-    puts "********************#{this_user.hashed_password}"
-    puts "********************************authentication worked"
     redirect "/"
   else
-    puts "********************************authentication didn't work"
     @errors = "Invalid login"
     @questions = Question.all
     erb :index
   end
+end
+
+get '/users/logout' do
+  session[:user_id] = nil
+  redirect "/"
+end
+
+post '/users' do
+  this_user = User.new(username: params[:username], email: params[:email], password: params[:password])
+  if this_user.save
+    session[:user_id] = this_user.id
+  else
+    @questions = Question.all
+  end
+  erb :index
 end
