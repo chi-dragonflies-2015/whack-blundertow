c9061683e5f879aa7faf5e972695e3ee96b072c3
Nick Siefken <nicks720@gmail.com>
2015-06-18 16:43:00 -0500

    up/down vote for questions and for answers

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,13 +1,17 @@
-get '/questions/:question_id/votes/:value' do
+post '/questions/:question_id/votes/:value' do
   if session[:user_id]
     this_user = User.find_by(id: session[:user_id])
     this_question = Question.find_by(id: params[:question_id])
     this_vote = Vote.new(value: params[:value])
-    if Vote.allowed_to_vote(this_user, this_question)
+    if true
+     # Vote.allowed_to_vote(this_user, this_question)
       this_vote.save
       this_question.votes << this_vote
-      this_user.save
-      redirect "/questions/#{params[:question_id]}"
+      this_user.votes << this_vote
+      # redirect "/questions/#{params[:question_id]}"
+      content_type :json
+      {votes: Vote.total(this_question)}.to_json
+
     else
       @errors = "You already voted on this issue"
       redirect "/"
@@ -18,15 +22,20 @@ get '/questions/:question_id/votes/:value' do
   end
 end
 
-get '/answers/:answer_id/votes/:value' do
+post '/answers/:answer_id/votes/:value' do
   if session[:user_id]
     this_user = User.find_by(id: session[:user_id])
     this_answer = Answer.find_by(id: params[:answer_id])
     this_vote = Vote.new(value: params[:value])
-    if Vote.allowed_to_vote(this_user, this_answer)
+    if true
+      # Vote.allowed_to_vote(this_user, this_answer)
       this_vote.save
       this_answer.votes << this_vote
-      redirect "/questions/#{this_answer.question_id}"
+      this_user.votes << this_vote
+      # redirect "/questions/#{this_answer.question_id}"
+      content_type :json
+      {votes: Vote.total(this_answer)}.to_json
+
     else
       @errors = "You already voted on this issue"
       redirect "/"
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,36 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $('.question_container').on('click', 'a', function(event){
+    event.preventDefault();
+    var ajxUrl = ($(this).attr("href"));
+    var request = $.ajax({
+                    url: ajxUrl,
+                    method: 'POST'
+    });
+    request.done(function(response){
+      $('#vote_for_questions').text(response["votes"]);
+    });
+  });
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $(".answer").on('click', 'a', function(event){
+    event.preventDefault();
+    var ajxUrl = ($(this).attr("href"));
+    var pId = ($(this).attr("class"));
+    var request = $.ajax({
+                    url: ajxUrl,
+                    method: 'POST'
+    });
+    request.done(function(response){
+      $("p[class = " + pId + "]").text(response["votes"]);
+    });
+
+
+
+  });
+
+
+
+
+
+
+
 });
