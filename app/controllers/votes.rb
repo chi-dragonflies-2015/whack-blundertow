eceff0f277b971ccf43401321093a1ec2c02682e
Michael Kunc <mjkunc@gmail.com>
2015-06-18 11:36:47 -0500

    some vote functionality

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,11 +1,12 @@
 get '/questions/:question_id/votes/:value' do
   if session[:user_id]
     this_user = User.find_by(id: session[:user_id])
-    this_question = Question.find_by(params[:question_id])
+    this_question = Question.find_by(id: params[:question_id])
     this_vote = Vote.new(value: params[:value])
     if Vote.allowed_to_vote(this_user, this_question)
-      this_vote.create
+      this_vote.save
       this_user.votes << this_vote
+      redirect "/Zoo"
     else
       @errors = "You already voted on this issue"
       redirect "/"
@@ -16,10 +17,10 @@ get '/questions/:question_id/votes/:value' do
   end
 end
 
-get '/answer/:answer_id/votes/:value' do
+get '/answers/:answer_id/votes/:value' do
   if session[:user_id]
     this_user = User.find_by(id: session[:user_id])
-    this_answer = Question.find_by(params[:question_id])
+    this_answer = Question.find_by(id: params[:question_id])
     this_vote = Vote.new(value: params[:value])
     if Vote.allowed_to_vote(this_user, this_answer)
       this_vote.create
