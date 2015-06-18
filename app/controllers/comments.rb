7c37f19cc16fc46151c8696dca2991203feab453
Michael Kunc & Nick Siefken & Peter Macaluso <pair+michaelkunc.nsiefken.pmacaluso3@devbootcamp.com>
2015-06-18 08:22:29 -0500

    best answer route

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -5,7 +5,7 @@ end
 post '/comments' do
   if session[:user_id]
     this_user = User.find_by(id: session[:user_id])
-    this_comment = Comment.new(body: params[:body])
+    this_comment = Comment.create(body: params[:body])
     this_user.comments << this_comment
     #use ajax to display this comment on the current item
     #no delegation required
