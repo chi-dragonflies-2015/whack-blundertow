a73f99f3fdc6e40f3fc0a71faa1a5191e0537d24
Nick Siefken <nicks720@gmail.com>
2015-06-18 19:13:09 -0500

    probably done with ajax

--- public/js/application.js
+++ public/js/application.js
@@ -22,11 +22,40 @@ $(document).ready(function() {
     request.done(function(response){
       $("p[class = " + pId + "]").text(response["votes"]);
     });
-
-
-
   });
 
+  $('#question_comment_button').on('click', function(event){
+    event.preventDefault();
+    var ajaxUrl = ($(this).parent().attr("action"));
+    var newComment = $('#new_question_comment').val();
+    var request = $.ajax({
+                    url: ajaxUrl,
+                    method: 'POST',
+                    data: {"body": newComment}
+    });
+    request.done(function(response){
+      $('#show_question_comments').prepend(response);
+    });
+  });
+
+  $(".answer_comment_button").on("click", function(event) {
+    event.preventDefault();
+    var formNum = $(this).parent();
+    var ajaxUrl = formNum.attr("action");
+    var thisClass = formNum.children().eq(0).attr("class");
+    var newComment = formNum.children().eq(0).val();
+    var request = $.ajax({
+                        url: ajaxUrl,
+                        method: "POST",
+                        data: {"body": newComment}
+    });
+
+    request.done(function(response) {
+      $("div ." + thisClass).prepend(response);
+
+    });
+
+  });
 
 
 
